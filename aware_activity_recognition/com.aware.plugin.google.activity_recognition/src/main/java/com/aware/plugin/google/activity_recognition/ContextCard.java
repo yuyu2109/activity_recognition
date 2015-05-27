1b19332e6a43ab4083f1a4f967c7ef7eb0d3f3ee
Romain Frigerio <romain.frigerio@ensimag.imag.fr>
2015-05-27 11:00:16 +0200

    Init commit

---
+++ aware_activity_recognition/com.aware.plugin.google.activity_recognition/src/main/java/com/aware/plugin/google/activity_recognition/ContextCard.java
@@ -0,0 +1,147 @@
+package com.aware.plugin.google.activity_recognition;
+
+import java.util.Calendar;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.aware.utils.Converters;
+import com.aware.utils.IContextCard;
+import com.google.android.gms.location.DetectedActivity;
+
+/**
+ * New Stream UI cards<br/>
+ * Implement here what you see on your Plugin's UI.
+ * @author denzilferreira
+ */
+public class ContextCard implements IContextCard {
+    public static final int IND_MAX = 20;
+    public ContextCard(){};
+	
+	public View getContextCard(Context context) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View mInflated = inflater.inflate(R.layout.layout, null);
+		
+		TextView still = (TextView) mInflated.findViewById(R.id.time_still);
+        TextView walking = (TextView) mInflated.findViewById(R.id.time_walking);
+        TextView biking = (TextView) mInflated.findViewById(R.id.time_biking);
+        TextView vehicle = (TextView) mInflated.findViewById(R.id.time_vehicle);
+
+        Calendar mCalendar = Calendar.getInstance();
+        mCalendar.setTimeInMillis(System.currentTimeMillis());
+
+        //Modify time to be at the begining of today
+        mCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        mCalendar.set(Calendar.MINUTE, 0);
+        mCalendar.set(Calendar.SECOND, 0);
+        mCalendar.set(Calendar.MILLISECOND, 0);
+
+        long now = mCalendar.getTime().getTime();
+
+        long lastTimeStill = Stats.getLastTimeStill(context.getContentResolver(), mCalendar.getTimeInMillis(), System.currentTimeMillis());
+        long lastTimeWalking = Stats.getLastTimeWalking(context.getContentResolver(), mCalendar.getTimeInMillis(), System.currentTimeMillis());
+        long lastTimeBiking = Stats.getLastTimeBiking(context.getContentResolver(), mCalendar.getTimeInMillis(), System.currentTimeMillis());
+        long lastTimeVehicle = Stats.getLastTimeVehicle(context.getContentResolver(), mCalendar.getTimeInMillis(), System.currentTimeMillis());
+
+        //Get stats for today
+        //still.setText(Converters.readable_elapsed(Stats.getLastTimeStill(context.getContentResolver(), mCalendar.getTimeInMillis(), System.currentTimeMillis())));
+        still.setText(Converters.readable_elapsed(getTime(now, lastTimeStill)));
+        walking.setText(Converters.readable_elapsed(getTime(now,lastTimeWalking)));
+        biking.setText(Converters.readable_elapsed(getTime(now, lastTimeBiking)));
+        vehicle.setText(Converters.readable_elapsed(getTime(now, lastTimeVehicle)));
+
+        for (int k = 0; k < IND_MAX; k++) {
+            Stats.getActivity(context.getContentResolver(), mCalendar.getTimeInMillis(), System.currentTimeMillis(), k);
+            setInfo(context, mInflated, now, k);
+        }
+
+		return mInflated;
+	}
+
+    private long getTime(long now, long time) {
+        if (time == 0)
+            return time;
+        return time - now;
+    }
+
+    private void setInfo(Context context, View mInflated, long now ,int indice) {
+        int image_resource = context.getResources().getIdentifier("icon_show" + indice, "id", context.getPackageName());
+        int text_resource = context.getResources().getIdentifier("textShow"  +indice, "id", context.getPackageName());
+
+        ImageView icon = (ImageView) mInflated.findViewById(image_resource);
+        TextView text = (TextView) mInflated.findViewById(text_resource);
+
+        switch (Plugin.type) {
+            case DetectedActivity.IN_VEHICLE:
+                if (Plugin.confidence >= 70)
+                    icon.setImageResource(R.drawable.ic_action_vehicle_own);
+                else
+                    icon.setImageResource(R.drawable.ic_action_vehicle);
+
+                text.setText("Vehicle at " + Converters.readable_elapsed(getTime(now, Plugin.time)) + " with confidence " + Plugin.confidence);
+                break;
+            case DetectedActivity.ON_BICYCLE:
+                if (Plugin.confidence >= 70)
+                    icon.setImageResource(R.drawable.ic_action_biking_own);
+                else
+                    icon.setImageResource(R.drawable.ic_action_biking);
+
+                text.setText("Bicycle at " + Converters.readable_elapsed(getTime(now, Plugin.time)) + " with confidence " + Plugin.confidence);
+                break;
+            case DetectedActivity.ON_FOOT:
+                if (Plugin.confidence >= 70)
+                    icon.setImageResource(R.drawable.ic_action_walking_own);
+                else
+                    icon.setImageResource(R.drawable.ic_action_walking);
+
+                text.setText("On foot at " + Converters.readable_elapsed(getTime(now, Plugin.time)) + " with confidence " + Plugin.confidence);
+                break;
+            case DetectedActivity.STILL:
+                if (Plugin.confidence >= 70)
+                    icon.setImageResource(R.drawable.ic_action_still_own);
+                else
+                    icon.setImageResource(R.drawable.ic_action_still);
+
+                text.setText("Still at " + Converters.readable_elapsed(getTime(now, Plugin.time)) + " with confidence " + Plugin.confidence);
+                break;
+            case DetectedActivity.UNKNOWN:
+                if (Plugin.confidence >= 50)
+                    icon.setImageResource(R.drawable.unknown_black_own);
+                else
+                    icon.setImageResource(R.drawable.unknown_grey_own);
+
+                text.setText("     Unknown at " + Converters.readable_elapsed(getTime(now, Plugin.time)) + " with confidence " + Plugin.confidence);
+                break;
+            case DetectedActivity.TILTING:
+                if (Plugin.confidence >= 70)
+                    icon.setImageResource(R.drawable.tilting_red_own);
+                else
+                    icon.setImageResource(R.drawable.tilting_own);
+
+                text.setText("Changing activity at " + Converters.readable_elapsed(getTime(now, Plugin.time)) + " with confidence " + Plugin.confidence);
+                break;
+            case DetectedActivity.RUNNING:
+                if (Plugin.confidence >= 70)
+                    icon.setImageResource(R.drawable.ic_action_walking_own);
+                else
+                    icon.setImageResource(R.drawable.ic_action_walking);
+
+                text.setText("Running at " + Converters.readable_elapsed(getTime(now, Plugin.time)) + " with confidence " + Plugin.confidence);
+                break;
+            case DetectedActivity.WALKING:
+                if (Plugin.confidence >= 70)
+                    icon.setImageResource(R.drawable.ic_action_walking_own);
+                else
+                    icon.setImageResource(R.drawable.ic_action_walking);
+
+                text.setText("Walking at " + Converters.readable_elapsed(getTime(now, Plugin.time)) + " with confidence " + Plugin.confidence);
+                break;
+            default:
+                text.setText("Error : activity code not found " + Plugin.current_activity);
+        }
+    }
+}
---
+++ aware_activity_recognition/com.aware.plugin.google.activity_recognition/src/main/java/com/aware/plugin/google/activity_recognition/Plugin.java
@@ -0,0 +1,125 @@
+
+package com.aware.plugin.google.activity_recognition;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.util.Log;
+
+import com.aware.Aware;
+import com.aware.Aware_Preferences;
+import com.aware.plugin.google.activity_recognition.Google_AR_Provider.Google_Activity_Recognition_Data;
+import com.aware.utils.Aware_Plugin;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
+
+public class Plugin extends Aware_Plugin implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+	public static String ACTION_AWARE_GOOGLE_ACTIVITY_RECOGNITION = "ACTION_AWARE_GOOGLE_ACTIVITY_RECOGNITION";
+	public static String EXTRA_ACTIVITY = "activity";
+	public static String EXTRA_CONFIDENCE = "confidence";
+
+	private static PendingIntent gARPending = null;
+	private static GoogleApiClient gARClient = null;
+
+	public static int current_activity = -1;
+	public static int current_confidence = -1;
+
+	public static int type = -1;
+	public static long time = -1;
+	public static long confidence = -1;
+
+    private static PowerManager.WakeLock wakelock;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		TAG = "AWARE::Google Activity Recognition";
+		DEBUG = Aware.getSetting(this, Aware_Preferences.DEBUG_FLAG).equals("true");
+
+		DATABASE_TABLES = Google_AR_Provider.DATABASE_TABLES;
+		TABLES_FIELDS = Google_AR_Provider.TABLES_FIELDS;
+		CONTEXT_URIS = new Uri[]{ Google_Activity_Recognition_Data.CONTENT_URI };
+		
+		CONTEXT_PRODUCER = new ContextProducer() {
+			@Override
+			public void onContext() {
+				Intent context = new Intent(ACTION_AWARE_GOOGLE_ACTIVITY_RECOGNITION);
+				context.putExtra(EXTRA_ACTIVITY, current_activity);
+				context.putExtra(EXTRA_CONFIDENCE, current_confidence);
+				sendBroadcast(context);
+			}
+		};
+
+		Aware.setSetting(getApplicationContext(), Settings.STATUS_PLUGIN_GOOGLE_ACTIVITY_RECOGNITION, true);
+		if( Aware.getSetting(getApplicationContext(), Settings.FREQUENCY_PLUGIN_GOOGLE_ACTIVITY_RECOGNITION).length() == 0 ) {
+			Aware.setSetting(getApplicationContext(), Settings.FREQUENCY_PLUGIN_GOOGLE_ACTIVITY_RECOGNITION, 60);
+		}
+
+        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        wakelock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG + " lock");
+        wakelock.acquire();
+
+		if ( ! is_google_services_available() ) {
+			Log.e(TAG,"Google Services activity recognition not available on this device.");
+			stopSelf();
+		} else {
+			gARClient = new GoogleApiClient.Builder(this)
+                    .addApi(ActivityRecognition.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+            gARClient.connect();
+
+            Intent gARIntent = new Intent();
+			gARIntent.setClassName(getPackageName(), getPackageName() + ".Algorithm");
+			gARPending = PendingIntent.getService(getApplicationContext(), 0, gARIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+		}
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		if( gARClient.isConnected() ) {
+            ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates( gARClient, Long.valueOf(Aware.getSetting(getApplicationContext(), Settings.FREQUENCY_PLUGIN_GOOGLE_ACTIVITY_RECOGNITION)) * 1000, gARPending );
+        }
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+        wakelock.release();
+        Aware.setSetting(getApplicationContext(), Settings.STATUS_PLUGIN_GOOGLE_ACTIVITY_RECOGNITION, false);
+
+		//we might get here if phone doesn't support Google Services
+		if ( gARClient != null && gARClient.isConnected() ) {
+			ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(gARClient, gARPending);
+		}
+	}
+
+	private boolean is_google_services_available() {
+		return (ConnectionResult.SUCCESS == GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext()));
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connection_result) {
+		Log.w(TAG,"Error connecting to Google's activity recognition services, will try again in 5 minutes");
+	}
+
+	@Override
+	public void onConnected(Bundle bundle) {
+        Log.i(TAG,"Connected to Google's Activity Recognition API");
+        Aware.startPlugin(this, getPackageName());
+	}
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.w(TAG,"Error connecting to Google's activity recognition services, will try again in 5 minutes");
+    }
+}
